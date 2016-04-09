a907cc8358ed58b7a5f43e50237bcca732299153
sabry <sabry.smk@gmail.com>
2016-04-09 22:56:21 +0300

    Add cloudKit sync and hardcode demo data for discover tab

---
+++ FoodPin/DiscoverTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DiscoverTableViewCell.swift
+//  FoodPin
+//
+//  Created by Александр Сабри on 09/04/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodPin/Restaurants.swift
@@ -0,0 +1,9 @@
+//
+//  Restaurants.swift
+//  FoodPin
+//
+//  Created by Александр Сабри on 09/04/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import Foundation
